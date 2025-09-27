08af20bdc8da9a0f36118fe6b7dfd4bc9ec1b173
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-27 20:12:20 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,5 @@
 <?php declare(strict_types=1);
 
 return [
-    'app_name' => 'MyFramework',
     'debug' => true,
-
-    // Language
-    'default_language' => 'auto',
-    'fallback_language' => 'en',
-    'supported_languages' => ['en', 'ru'],
-    'auto_detect_language' => true,
 ];
