7d0665fd9006214b25db6221fa22cbec53f9377b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-27 19:52:34 +0300

    refactor: Some improvements for language functionality

--- config/config.php
+++ config/config.php
@@ -3,4 +3,10 @@
 return [
     'app_name' => 'MyFramework',
     'debug' => true,
+
+    // Language
+    'default_language' => 'auto',
+    'fallback_language' => 'en',
+    'supported_languages' => ['en', 'ru'],
+    'auto_detect_language' => true,
 ];
