344d0f85360598fff4b8d7249cd7fd34d0f4e6ec
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-27 16:29:37 +0300

    feat: add `Config` class for managing configuration files

--- core/App.php
+++ core/App.php
@@ -4,19 +4,9 @@ namespace Core;
 
 class App
 {
-    protected static array $config = [];
-
     public static function init(): void
     {
         // Load configuration files
-        foreach (glob(ROOT . '/config/*.php') as $file) {
-            $key = basename($file, '.php');
-            self::$config[$key] = require $file;
-        }
-    }
-
-    public static function config(string $key, $default = null)
-    {
-        return self::$config[$key] ?? $default;
+        Config::load(ROOT . '/config');
     }
 }
