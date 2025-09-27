d90e5f67328f731899cc4eb417b5afe9d617e20e
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-27 18:20:23 +0300

    Updated

---
+++ config/framework.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+return [
+    'name' => 'Alexandria',
+    'homepage' => '',
+    'version' => '1.0.0-alpha.1'
+];
--- core/helpers.php
+++ core/helpers.php
@@ -1,6 +1,13 @@
 <?php declare(strict_types=1);
 
-function config(string $key, $default = null): mixed
+/**
+ * Get configuration value (shortcut for Core\Config::get)
+ *
+ * @param string $key Configuration key
+ * @param mixed $default Default value if key does not exist
+ * @return mixed
+ */
+function config(string $key, mixed $default = null): mixed
 {
     return \Core\Config::get($key, $default);
 }
