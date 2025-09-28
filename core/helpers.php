5d23dc8bad60335a43056f40dec5e47e927e7ea6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-28 09:35:31 +0300

    Update helpers.php

--- core/helpers.php
+++ core/helpers.php
@@ -1,6 +1,13 @@
 <?php declare(strict_types=1);
 
-function config(string $key, $default = null): mixed
+/**
+ * Get configuration value (shortcut for Core\Config::get)
+ *
+ * @param string $key Configuration key
+ * @param mixed $default Default value
+ * @return mixed
+ */
+function config(string $key, mixed $default = null): mixed
 {
     return \Core\Config::get($key, $default);
 }
@@ -17,6 +24,13 @@ function __(string $key, array $params = []): string
     return \Core\Lang::get($key, $params);
 }
 
+/**
+ * Get environment variable (shortcut for Core\Env::get or $_SERVER)
+ *
+ * @param string $key Variable name
+ * @param mixed $default Default value
+ * @return mixed
+ */
 function env(string $key, mixed $default = null): mixed
 {
     if (class_exists('\Core\Env')) {
