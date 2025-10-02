53a8ec4970ec49d7acad8364c99fa26fbe884ba8
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-10-02 11:46:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: '.',
+  publicDir: false,
   build: {
     outDir: 'public/build',
     manifest: true,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         app: resolve(__dirname, 'resources/js/app.js'),
