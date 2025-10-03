b3c18321f8d954a6ccfccc178d71308dd1ebfb68
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-10-03 12:28:18 +0300

    Updated

---
+++ database/migrations/2025_10_03_120000_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Core\Database\Migrations\Migration;
+use Core\Database\Schema\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function ($table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('remember_token', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+}
+
